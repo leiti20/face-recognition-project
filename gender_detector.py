ca7ad6196cf91ab78fac3d851fba65a1abc9901a
leiti20 <leiticiam3292@gmail.com>
2026-02-09 21:31:01 +0100

    modifier

--- gender_detector.py
+++ gender_detector.py
@@ -9,12 +9,10 @@ class GenderDetector:
 
         self.genderList = ['Homme', 'Femme']
 
-    # Prédire le genre à partir de l'image du visage
     def predict(self, face_img):
         blob = cv.dnn.blobFromImage(face_img, 1.0, (227, 227),
                                      (78.4263377603, 87.7689143744, 114.895847746),
                                      swapRB=False)
-        # Passer l'image à travers le réseau pour obtenir les prédictions
         self.net.setInput(blob)
         preds = self.net.forward()
         return self.genderList[preds[0].argmax()]
